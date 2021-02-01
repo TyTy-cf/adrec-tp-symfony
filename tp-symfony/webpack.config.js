4e7a7a33c49f69140ce5ec2402a540886cebf4e9
Karim HADJ-ABDELKADER <karim-hadj@hotmail.fr>
2021-02-01 10:55:37 +0100

    Fixed exception

--- tp-symfony/webpack.config.js
+++ tp-symfony/webpack.config.js
@@ -17,10 +17,12 @@ Encore
     /*
      * ENTRY CONFIG
      *
-     * Each entry will result in one JavaScript file (e.g. app.js)
-     * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
+     * Each entry will result in one JavaScript file (e.g. scripts.js)
+     * and one CSS file (e.g. app.scss) if your JavaScript imports CSS.
      */
-    .addEntry('app', './assets/app.js')
+    .addEntry('styles', './assets/styles.scss')
+    .addEntry('scripts', './assets/scripts.js')
+
 
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
     .enableStimulusBridge('./assets/controllers.json')
@@ -56,7 +58,7 @@ Encore
     })
 
     // enables Sass/SCSS support
-    //.enableSassLoader()
+    .enableSassLoader()
 
     // uncomment if you use TypeScript
     //.enableTypeScriptLoader()
